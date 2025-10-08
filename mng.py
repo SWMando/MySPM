455fcf77f1b5b2ebee30b0b839bd0a6c95cd1e02
SWMando <akbarov.salim@gmail.com>
2025-10-09 00:06:17 +0300

    Edit Login added

--- mng.py
+++ mng.py
@@ -17,7 +17,7 @@ from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
 ### Global variables
 welcome_msg = "Welcome to the My$PM\n"
-guide = """To create an entry, you need to fill in site, username and enter the password for the account. Site can be the short domain name of the site to which the account is related to, for example, \"facebook.com\". The username can be whatever you use for login, it can be email address, your name, or username. Finally the password, obviously, the password itself."""
+guide = """To create an entry, you need to fill in:\n\n1. Entity Name - should be short and unique name which can be used to select between two accounts on the same site.\n2. Site - can be the short domain name of the site to which the account is related to, for example, \"facebook.com\".\n3. Username - can be whatever you use for login, it can be email address, your username, or actual name.\n4. Password length - how long the password you want to have.\n\nFor security purposes, I believe it would be better that you would not be able to use your own password,\ninstead the password should be generated.\nIn My$PM you can generate passwords with length 10-22 characters.\n\nIF POSSIBLE TRY TO GENERATE AS LONG OF A PASSWORD AS POSSIBLE!!!"""
 find_opt = {
     "1": "Show entry",
     "2": "Search again",
@@ -25,12 +25,13 @@ find_opt = {
 }
 find_action = {
     "1": "Copy password",
-    "2": "Delete login"
+    "2": "Delete login",
+    "3": "Edit Login"
 }
+edit_msg = """Please enter only the parts which you wish to change.\nIf you do not want to change somefield, just leave it empty"""
 opt = {
     "1": "Create new Login",
     "2": "Find Login",
-    "3": "Edit Login",
     ".": "To leave press"
 }
 
@@ -62,18 +63,8 @@ class DB:
                 CREATE TABLE IF NOT EXISTS logins (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     user_id INTEGER NOT NULL,
-                    entity_name TEXT NOT NULL,
-                    username TEXT NOT NULL,
-                    password_encr BLOB NOT NULL,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-                )
-            ''')
-            c.execute('''
-                CREATE TABLE IF NOT EXISTS trash (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    user_id INTEGER NOT NULL,
-                    entity_name TEXT NOT NULL,
+                    entity_name TEXT UNIQUE NOT NULL,
+                    site_name TEXT NOT NULL,
                     username TEXT NOT NULL,
                     password_encr BLOB NOT NULL,
                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -185,19 +176,31 @@ def password_gen(length):
 
 
 def create_login(user_id, key):
-    print(f"{guide}\n")
-    site = input("Please enter the site: ")
-    username = input("Please enter the username: ")
-    length = input("Please enter password length(10-26): ")
-    password = password_gen(int(length))
-    # Fernet algorythm code snipplet reference: https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
-    # https://stackoverflow.com/questions/27335726/how-do-i-encrypt-and-decrypt-a-string-in-python
-    f = Fernet(base64.urlsafe_b64encode(key))
-    try:
-        password_encr = f.encrypt(password.encode())
-        db.run_change('''INSERT INTO logins (user_id, entity_name, username, password_encr) VALUES (?, ?, ?, ?)''', params=(user_id, site, username, password_encr))
-    except KeyboardInterrupt:
-        input("Please press enter to continue... ")
+    while True:
+        clear()
+        try:
+            print(f"{guide}\n")
+            entity_name = input("Please give a short and unique name for the entry: ")
+            site = input("Please enter the site: ")
+            username = input("Please enter the username: ")
+            length = input("Please enter password length(10-26): ")
+            password = password_gen(int(length))
+            # Fernet algorythm code snipplet reference: https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
+            # https://stackoverflow.com/questions/27335726/how-do-i-encrypt-and-decrypt-a-string-in-python
+            f = Fernet(base64.urlsafe_b64encode(key))
+            password_encr = f.encrypt(password.encode())
+            db.run_change('''INSERT INTO logins (user_id, entity_name, site_name, username, password_encr) VALUES (?, ?, ?, ?, ?)''', params=(user_id, entity_name, site, username, password_encr))
+            break
+        except sqlite3.IntegrityError:
+            clear()
+            input("Entered Entity Name is already used. Please try to use something different! ")
+        except KeyboardInterrupt:
+            clear()
+            break
+            input("Please press enter to continue... ")
+        except EOFError:
+            clear()
+            exit()
 
 
 def copy_login(key, password_encr):
@@ -212,12 +215,36 @@ def copy_login(key, password_encr):
         input("Please press enter to continue... ")
 
 
-def delete_login(entry_id):
+def delete_login(entity_name):
     clear()
     ays = input("Are you sure that you want to delete this? Ones deleted, you will not be able to recover this(y/n): ")
     if ays == "n":
         return
-    db.run_change('''DELETE FROM logins WHERE id = ?''', params=(entry_id,))
+    db.run_change('''DELETE FROM logins WHERE entity_name = ?''', params=(entity_name,))
+
+
+def edit_login(key, entity_name):
+    print(edit_msg)
+    new_entity_name = input("Entity Name: ")
+    new_site = input("Site: ")
+    new_username = input("Username: ")
+    new_password_len = input("Password Length: ")
+
+    ays = input("Are you sure that you want to apply these changes? Ones applied, you will not be able to revert this(y/n): ")
+    if ays == "n":
+        return
+
+    if new_entity_name != "":
+        db.run_change('''UPDATE logins SET entity_name = ? WHERE entity_name = ?''', params=(new_entity_name, entity_name))
+    if new_site != "":
+        db.run_change('''UPDATE logins SET site_name = ? WHERE entity_name = ?''', params=(new_site, entity_name))
+    if new_username != "":
+        db.run_change('''UPDATE logins SET username = ? WHERE entity_name = ?''', params=(new_username, entity_name))
+    if new_password_len != "":
+        password = password_gen(int(new_password_len))
+        f = Fernet(base64.urlsafe_b64encode(key))
+        password_encr = f.encrypt(password.encode())
+        db.run_change('''UPDATE logins SET password_encr = ? WHERE entity_name = ?''', params=(password_encr, entity_name))
 
 
 def find_login(key):
@@ -225,8 +252,8 @@ def find_login(key):
         clear()
         try:
             site = input("Please enter the site name you are looking for: ")
-            query = db.run_query('''SELECT id, entity_name, username FROM logins WHERE entity_name LIKE ?''', params=(f"%{site}%",))
-            headers = ["", "Site", "Username"]
+            query = db.run_query('''SELECT entity_name, site_name, username FROM logins WHERE site_name LIKE ?''', params=(f"%{site}%",))
+            headers = ["Entity Name", "Site", "Username"]
             print(tabulate(query, headers=headers, tablefmt="pretty"))
             for x,y in find_opt.items():
                 if x == ".":
@@ -236,9 +263,9 @@ def find_login(key):
             find_choice = input("Please choose an option: ")
             match find_choice:
                 case "1":
-                    entry_id = input("Please choose an entry: ")
+                    entity_name = input("Please choose an entry(Entity Name): ")
                     clear()
-                    query = db.run_query('''SELECT entity_name, username, password_encr FROM logins WHERE id = ?''', params=(entry_id,))
+                    query = db.run_query('''SELECT site_name, username, password_encr FROM logins WHERE entity_name = ?''', params=(entity_name,))
                     site, username, password_encr = query[0]
                     while True:
                         clear()
@@ -255,9 +282,11 @@ def find_login(key):
                         match find_action_choice:
                             case "1":
                                 copy_login(key, password_encr)
-                                break
                             case "2":
-                                delete_login(entry_id)
+                                delete_login(entity_name)
+                                break
+                            case "3":
+                                edit_login(key, entity_name)
                                 break
                             case ".":
                                 break
@@ -273,7 +302,8 @@ def find_login(key):
             break
 
 
-def master_login():
+
+def master_auth():
     while True:
         clear()
         username = input("Username: ")
@@ -298,7 +328,7 @@ def main():
         print("Please create a Master User")
         create_master_user()
         clear()
-    user_id, encr_key = master_login()
+    user_id, encr_key = master_auth()
     while True:
         clear()
         print(welcome_msg)
@@ -316,9 +346,6 @@ def main():
             case "2":
                 clear()
                 find_login(encr_key)
-            case "3":
-                clear()
-                input("Soon!")
             case ".":
                 clear()
                 break
